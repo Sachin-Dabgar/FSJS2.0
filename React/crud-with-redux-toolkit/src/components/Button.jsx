491dba1432a952b6210aa4ecd46ce0df99edced8
Sachin-Dabgar <bosscoder1325@gmail.com>
2023-05-18 08:07:08 +0530

    crud app

--- React/crud-with-redux-toolkit/src/components/Button.jsx
+++ React/crud-with-redux-toolkit/src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({ onClick, children }) => {
   return (
     <button
-      className="bg-[brown] text-white font-[600] py-2 px-6 my-10 rounded hover:bg-[#942424]"
+      className="bg-[#312d2d] text-white font-[600] py-2 px-6 my-10 rounded hover:bg-[#2c2a2a]"
       onClick={onClick}
     >
       {children}
