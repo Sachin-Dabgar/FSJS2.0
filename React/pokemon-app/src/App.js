34f7a5aa76c550cbc9f6ff04bb60760a962ee378
Sachin-Dabgar <bosscoder1325@gmail.com>
2023-05-18 08:30:48 +0530

    pockemon

--- React/pokemon-app/src/App.js
+++ React/pokemon-app/src/App.js
@@ -1,68 +1,86 @@
-import React, { useState } from 'react';
-import Axios from 'axios';
-import './App.css';
-import PokeCard from './PokeCard';
-import LoadingSpinner from './LoadingSpinner';
+import React, { useState } from "react";
+import Axios from "axios";
+import "./App.css";
+import PokeCard from "./PokeCard";
+import LoadingSpinner from "./LoadingSpinner";
 
 const App = () => {
-  const [query, setQuery] = useState('');
-  const [pokemon, setPokemon] = useState([]);
-  const [loading, setLoading] = useState(false);
+    const [query, setQuery] = useState("");
+    const [pokemon, setPokemon] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-  const url = `https://pokeapi.co/api/v2/pokemon/${query}`;
+    const url = `https://pokeapi.co/api/v2/pokemon/${query}`;
 
-  const getPokemon = async () => {
-    try {
-      setLoading(true);
-      const result = await Axios.get(url);
-      setPokemon([result.data]);
-      console.log(result.data);
-      setLoading(false);
-    } catch (error) {
-      // setPokemon(false);
-      alert('Could not find Pokemon😢, Try again!');
-      setLoading(false);
-      console.error(error);
-    }
-  };
+    const getPokemon = async () => {
+        try {
+            setLoading(true);
+            const result = await Axios.get(url);
+            setPokemon([result.data]);
+            console.log(result.data);
+            setLoading(false);
+        } catch (error) {
+            // setPokemon(false);
+            alert("Could not find Pokemon😢, Try again!");
+            setLoading(false);
+            console.error(error);
+        }
+    };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    getPokemon();
-  };
+    const onSubmit = (e) => {
+        e.preventDefault();
+        getPokemon();
+    };
 
-  return (
-    <div className="app">
-      <img
-        className="logo"
-        src="https://seeklogo.com/images/P/Pokemon-logo-497D61B223-seeklogo.com.png"
-        alt="pokemon-logo"
-      />
-      <form className="app-form" onSubmit={onSubmit}>
-        <input
-          type="text"
-          className="app-input"
-          placeholder="Search Pokemon by id or name..."
-          value={query.toLowerCase()}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+    return (
+        <div className="app">
+            <img
+                className="logo"
+                src="https://seeklogo.com/images/P/Pokemon-logo-497D61B223-seeklogo.com.png"
+                alt="pokemon-logo"
+            />
+            <form
+                className="app-form"
+                onSubmit={onSubmit}
+            >
+                <input
+                    type="text"
+                    className="app-input"
+                    placeholder="Search Pokemon by id or name..."
+                    value={query.toLowerCase()}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
 
-        <button type="submit" onClick={getPokemon} className="app-submit">
-          Get
-        </button>
-      </form>
+                <button
+                    type="submit"
+                    onClick={getPokemon}
+                    className="app-submit"
+                >
+                    Get
+                </button>
+            </form>
 
-      {loading ? (
-        <LoadingSpinner />
-      ) : (
-        <div className="app-poke-container">
-          {pokemon?.map((pokeItem, index) => {
-            return <PokeCard key={index} poke={pokeItem} />;
-          })}
+            {loading ? (
+                <LoadingSpinner />
+            ) : (
+                <div className="app-poke-container">
+                    {pokemon.length === 0 ? (
+                        <div style={{ color: "white" }}>
+                            Hint : search pockemon bulbasaur
+                        </div>
+                    ) : (
+                        pokemon?.map((pokeItem, index) => {
+                            return (
+                                <PokeCard
+                                    key={index}
+                                    poke={pokeItem}
+                                />
+                            );
+                        })
+                    )}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default App;
